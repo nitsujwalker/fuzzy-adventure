2531f6dac591df1aa9d4a59230f0d201d37dc758
nitsujwalker <untillife@gmail.com>
2015-03-04 23:50:57 -0600

    Update Fibonacci.py

--- Fibonacci.py
+++ Fibonacci.py
@@ -8,29 +8,3 @@ def sequence(n):
 		
 				
 sequence(10)
-
-"""def fibSequence(n):
-	a, b = 0, 1
-	for i in range(1, n+1):
-		print a
-		a, b = b, a+b
-	return
-	
-fibSequence(2000)"""
-		
-		
-"""
-def sequence(n):
-	x = 1
-	previousa = 0
-	previousx = 0
-	count = 0
-	while count < (n+1):
-		print (x)
-		previousx = x
-		x += previousa
-		previousa = previousx
-		count +=1
-		
-				
-sequence(10)"""
